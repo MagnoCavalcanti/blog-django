3d67da33f4a6109183ddace39ed5e1bb3b3de3a4
Wpabloz <wesleypablo1311@gmail.com>
2025-09-15 17:38:57 -0300

    feat(database): cria as tabelas do projeto

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+    username = models.CharField(max_length=100, null=False)
+    email = models.EmailField(null=False)
+
+    def str(self):
+        return self.username
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, null=False)
+    content = models.TextField(null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_by = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.title
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(max_length=300, null=False)
+    created_by = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.author} comentou no post {self.post}"
