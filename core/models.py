85fd3dc539c0316d081d414d9720dfc8fc92370a
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-10-28 15:55:01 -0300

    fix(Models): implementa o relacionamento N:N entra Post e User

--- core/models.py
+++ core/models.py
@@ -19,12 +19,14 @@ class SoftDeletableMixin(models.Model):
 
 
 
+
+
 class Post(SoftDeletableMixin, models.Model):
     title = models.CharField(max_length=200, null=False)
     content = models.TextField(null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     created_by = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='posts_curtidos', blank=True)
 
     def __str__(self):
         return self.title
